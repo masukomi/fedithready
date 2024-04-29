f1bb0b3af377ef662ee76eca8daf0424a3af31db
masukomi <masukomi@masukomi.org>
2024-04-29 13:57:45 -0400

    refactoring to support impending sentence end breaking

--- script.js
+++ script.js
@@ -22,7 +22,12 @@ $(document).ready(function() {
         return $('#paginationCheckbox').prop('checked');
     }
 
+    function attemptSentenceEndings() {
+        return $('#sentenceEndingsCheckbox').prop('checked');
+    }
+
     function getPaginationText(index, totalPosts) {
+        if (index === undefined || totalPosts === undefined) { return "" };
         return `\n🧵${index + 1}/${totalPosts}`;
     }
 
@@ -31,22 +36,28 @@ $(document).ready(function() {
     // We need this to see how many characters the pagination will take up at the end
     // of each post.
     function getNaiveChunkCount(text){
+        if (text === undefined || text.length == 0){ return 0; }
         let unmodifiedCharLimit = getUnmodifiedCharacterLimit();
         // example of what we're doing:
         // "abcd".match(new RegExp(r1, "g")) => Array [ "abc", "d" ]
         let r1 = `.{1,${unmodifiedCharLimit}}`;
 
-        let naiveChunkCount = text.match(new RegExp(r1, "g")).length;
+        return text.match(new RegExp(r1, "g")).length;
     }
 
     function getPaginationTextLength(index, totalPosts) {
         if (!isPaginationEnabled()){return 0;}
 
+        return getTrueTextLength(getPaginationText(index, totalPosts));
+    }
+
+    function getTrueTextLength(text){
+        if (text === undefined || text.length == 0){return 0};
         // Begin the dark magic required to convince JavaScript to count
         // characters instead of codepoints.
         // example: "🧵 1/10".length should be 6 but the emoji makes it 7
-        // worse: "👩‍❤️‍💋‍👩".length === 11 because... JavaScript
-        return [...getPaginationText(index, totalPosts)].length;
+        // worse: "👩‍❤️‍💋‍👩".length === 11 because JavaScript's got Load Bearing Bugs
+        return [...text].length;
     }
     function getUnmodifiedCharacterLimit() {
         let limit = parseInt($('#charLimit').val(), 10);
@@ -57,6 +68,55 @@ $(document).ready(function() {
         return limit;
     }
 
+
+    function findSlicePoint(text, maxChars, currentChunk, maxChunks) {
+        // maxAbandonableChars is an arbitrary number of characters that
+        // we're willing to sacrifice at the end of a chunk
+        // in order to improve readability. This number may
+        // need to be tweaked with usage.
+        const maxAbandonableChars = 60;
+        let charLimit = maxChars;
+        if (isPaginationEnabled()){
+            charLimit -= getPaginationTextLength(currentChunk, maxChunks);
+        }
+        console.log(`XXX charLimit: ${charLimit} maxChars: ${maxChars}`);
+        if (getTrueTextLength(text) <= charLimit) {
+            // the current section of this chunk of the manual chunk
+            // is already shorter than the max
+            return text.length; // false length for splitting purposes
+        }
+
+        let sliceEnd = charLimit;
+        // we could do something fancy with regexp to get the last
+        // space including tabs and other not newline things
+        // but it's just not worth the cost.
+        let lastSpace = text.lastIndexOf(" ", sliceEnd);
+        // if there are no spaces then the end of the text
+        if (lastSpace == -1){lastSpace = text.length;}
+        let lastNewLine = text.lastIndexOf("\n", sliceEnd);
+        if (lastNewLine == -1){ lastNewLine = lastSpace; }
+        let difference = lastSpace - lastNewLine;
+
+        if (difference > 0){
+
+
+            // backtrack to the last newline or space because we don't want
+            // to split in the middle of a word.
+            //
+            // Apologies to folks writing Chinese and other languages
+            // that don't need spaces. I dunno what to do for you.
+            if (difference < maxAbandonableChars){
+                // it's nicer to break on a newline near the end
+                // than a space in the middle of a sentence that's closer
+                // to the max characters.
+
+                sliceEnd = lastNewLine;
+            }
+        } else {
+            sliceEnd = lastSpace;
+        }
+        return sliceEnd;
+    }
 
     function splitText(text) {
         if (text === undefined || text === ""){return [];}
@@ -78,58 +138,25 @@ $(document).ready(function() {
             //
             //     blah blah
             manualChunk = manualChunk.trim();
-            while (manualChunk.length > 0) {
-                let charLimit = unmodifiedCharLimit;
-                if (isPaginationEnabled()){
-                    charLimit -= getPaginationTextLength(chunks.length + 1, naiveChunkCount);
-                }
+            while (manualChunk.length > 0) { // true length doesn't matter here
+                let slicePoint = findSlicePoint(
+                                    manualChunk,
+                                    unmodifiedCharLimit,
+                                    chunks.length + 1,
+                                    naiveChunkCount
+                                );
 
-                if (manualChunk.length <= charLimit) {
-                    // the current section of this chunk of the manual chunk
-                    // is already shorter than the max
+                if (slicePoint == manualChunk.length){
                     chunks.push(manualChunk);
                     break;
-                    // exit the sub-chunking while loop but
-                    // not the greater loop over all manual chunks
                 }
 
-                let sliceEnd = charLimit;
-                // we could do something fancy with regexp to get the last
-                // space including tabs and other not newline things
-                // but it's just not worth the cost.
-                let lastSpace = manualChunk.lastIndexOf(" ", sliceEnd);
-                let lastNewLine = manualChunk.lastIndexOf("\n", sliceEnd);
-                if (lastNewLine == -1){ lastNewLine = lastSpace }
-                let difference = lastSpace - lastNewLine;
-
-                // maxDifference is an arbitrary number of characters that
-                // we're willing to sacrifice at the end of a chunk
-                // in order to improve readability. This number may
-                // need to be tweaked with usage.
-                const maxDifference = 60;
-
-
-                // backtrack to the last newline or space because we don't want
-                // to split in the middle of a word.
-                //
-                // Apologies to folks writing Chinese and other languages
-                // that don't need spaces. I dunno what to do for you.
-                if (difference < maxDifference){
-                    // it's nicer to break on a newline near the end
-                    // than a space in the middle of a sentence that's closer
-                    // to the max characters.
-
-                    sliceEnd = lastNewLine;
-                } else if (lastSpace !== -1) {
-                    sliceEnd = lastSpace;
-                }
-
-
-                let startChunk = manualChunk.slice(0, sliceEnd);
+                console.log(`XXX slicing at ${slicePoint} out of ${manualChunk.length}`);
+                let startChunk = manualChunk.slice(0, slicePoint);
                 chunks.push(startChunk);
                 // replace the think we're chunking with everything
                 // after the chunk we just made.
-                manualChunk = manualChunk.slice(sliceEnd).trim();
+                manualChunk = manualChunk.slice(slicePoint);
 
             }
         });
